45e1173392802c726243b8b6907a63080fda17c1
Isuru Bandaranayake <isbana.git@gmail.com>
2021-01-14 14:53:29 +0530

    Add drawCube

--- Project/main.cpp
+++ Project/main.cpp
@@ -21,6 +21,7 @@ bool showGrid = true;
 void init() {
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_CULL_FACE);
 }
 
 void drawAxes() {
@@ -59,6 +60,52 @@ void drawGrid() {
 		glVertex3f(ext, yGrid, line);
 		glVertex3f(-ext, yGrid, line);
 	}
+	glEnd();
+}
+
+void drawCube(GLfloat x, GLfloat y, GLfloat z, GLfloat w, GLfloat l, GLfloat h) {
+	glBegin(GL_QUADS);
+	// TOP
+	glColor3f(1.0, 1.0, 0.5);
+	glVertex3f(x, y + h, z);
+	glVertex3f(x, y + h, z + l);
+	glVertex3f(x + w, y + h, z + l);
+	glVertex3f(x + w, y + h, z);
+
+	// BOTTOM
+	glColor3f(0.5, 1.0, 1.0);
+	glVertex3f(x, y, z);
+	glVertex3f(x + w, y, z);
+	glVertex3f(x + w, y, z + l);
+	glVertex3f(x, y, z + l);
+
+	// FRONT
+	glColor3f(1.0, 0.5, 0.5);
+	glVertex3f(x, y, z + l);
+	glVertex3f(x + w, y, z + l);
+	glVertex3f(x + w, y + h, z + l);
+	glVertex3f(x, y + h, z + l);
+
+	// BACK
+	glColor3f(1.0f, 0.5f, 1.0f);
+	glVertex3f(x, y, z);
+	glVertex3f(x, y + h, z);
+	glVertex3f(x + w, y + h, z);
+	glVertex3f(x + w, y, z);
+
+	// LEFT
+	glColor3f(0.5, 1.0, 0.5);
+	glVertex3f(x, y, z);
+	glVertex3f(x, y, z + l);
+	glVertex3f(x, y + h, z + l);
+	glVertex3f(x, y + h, z);
+
+	// RIGHT
+	glColor3f(0.5, 0.5, 1.0);
+	glVertex3f(x + w, y, z);
+	glVertex3f(x + w, y + h, z);
+	glVertex3f(x + w, y + h, z + l);
+	glVertex3f(x + w, y, z + l);
 	glEnd();
 }
 
